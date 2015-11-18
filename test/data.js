57c037087e09796714bea6b6f2255a257b26b439
Jason Sebring <mail@jasonsebring.com>
2015-11-17 22:00:40 -0800

    products update

---
+++ test/data.js
@@ -0,0 +1,13 @@
+'use strict';
+
+let mongoose = require('glut-models').mongoose;
+let models = require('glut-models').models;
+
+module.exports = {
+	setup: function() {
+		this.cleanup();
+	},
+	cleanup: function() {
+
+	}
+};
