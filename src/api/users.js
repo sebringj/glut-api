470e1aa0970cee084b3783b2c008949fdc99be74
Jason Sebring <mail@jasonsebring.com>
2015-11-18 11:06:01 -0800

    base api added

--- src/api/users.js
+++ src/api/users.js
@@ -8,36 +8,36 @@ var cors = require('cors');
 
 router.get('/', cors(), security.auth(), security.authAdmin(), function(req, res) {
   User.find({}).sort({ modified: -1 }).exec()
-	.then(function(docs) {
-		res.json(docs);
-	})
-	.catch(function(err) {
-		res.status(500).send('server error');
-	});
-});
-
-router.post('/', cors(),
-function(req, res, next) {
-	let roles = _.get(req, 'body.roles', []);
-	// if admin and new user is admin, then ensure admin is doing this
-	if (Array.isArray(roles) && roles.indexOf('admin') > -1) {
-		security.auth()(req, res, function() {
-			security.authAdmin()(req, res, next);
-		});
-	} else {
-		next();
-	}
-},
-function(req, res) {
-	let user = new User(req.body);
-  user.save()
-  .then(function(doc) {
-    res.json({ user: doc });
+  .then(function(docs) {
+    res.json(docs);
   })
   .catch(function(err) {
     res.status(500).send('server error');
   });
 });
+
+router.post('/', cors(),
+  function(req, res, next) {
+    let roles = _.get(req, 'body.roles', []);
+    // if admin and new user is admin, then ensure admin is doing this
+    if (Array.isArray(roles) && roles.indexOf('admin') > -1) {
+      security.auth()(req, res, function() {
+        security.authAdmin()(req, res, next);
+      });
+    } else {
+      next();
+    }
+  },
+  function(req, res) {
+    let user = new User(req.body);
+    user.save()
+    .then(function(doc) {
+      res.json({ user: doc });
+    })
+    .catch(function(err) {
+      res.status(500).send('server error');
+    });
+  });
 
 router.put('/:id', cors(), security.auth(), function(req, res) {
   User.findOneAndUpdate({ _id: req.params.id }, req.body).exec()
--- src/api/variants.js
+++ src/api/variants.js
@@ -1,10 +1,50 @@
 'use strict';
 
-var router = require('express').Router();
-var Variant = require('glut-models').models.Variant;
+let router = require('express').Router();
+let Variant = require('glut-models').models.Variant;
+let security = require('../security');
+let _ = require('lodash');
+var cors = require('cors');
 
-router.get('/', function(req, res) {
-  res.status(500).send('not implemented');
+router.get('/', cors(), security.auth(), security.authAdmin(), function(req, res) {
+  Variant.find({}).exec()
+  .then(function(docs) {
+    res.json(docs);
+  })
+  .catch(function(err) {
+    res.status(500).send('server error');
+  });
+});
+
+router.post('/', cors(), security.auth(), security.authAdmin(), function(req, res) {
+  let variant = new Variant(req.body);
+  variant.save()
+  .then(function(doc) {
+    res.json({ variant: doc });
+  })
+  .catch(function(err) {
+    res.status(500).send('server error');
+  });
+});
+
+router.put('/:id', cors(), security.auth(), function(req, res) {
+  Variant.findOneAndUpdate({ _id: req.params.id }, req.body).exec()
+  .then(function(doc) {
+    res.json({ variant: doc });
+  })
+  .catch(function(err) {
+    res.status(500).send('server error');
+  });
+});
+
+router.delete('/:id', cors(), security.auth(), security.authAdmin(), function(req, res) {
+  Variant.findOneAndRemove({ _id: req.params.id }).exec()
+  .then(function(doc) {
+    res.json({ variant: doc });
+  })
+  .catch(function(err) {
+    res.status(500).send('server error');
+  });
 });
 
 module.exports = router;
