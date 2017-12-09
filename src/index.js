5e5bfe30d91ddfdd86121ff3bc62386de9666376
Jason Sebring <mail@jasonsebring.com>
2017-12-08 20:17:29 -0800

    ssl ports

--- src/index.js
+++ src/index.js
@@ -17,13 +17,6 @@ app.use(bodyParser.json());
 app.use(cors());
 app.use('/api', require('./api'));
 
-const lex = require('greenlock-express').create({
-  server: 'production',
-  email: config.letsEncrypt.email,
-  agreeTos: true,
-  approveDomains: config.letsEncrypt.domains
-});
-
 http.createServer(app).listen(config.port);
 if (443)
   https.createServer({
